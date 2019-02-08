4c8a45d5edd599a3cdef28e0a4b3f6370206935b
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-07 23:53:25 -0500

    added test case to check addition and threatening ability so far

--- Driver.java
+++ Driver.java
@@ -2,5 +2,7 @@ public class Driver{
   public static void main(String[] args) {
     QueenBoard m = new QueenBoard(8);
     System.out.println(m);
+    m.addQueen(2,1); //currently should block off threatened same row.
+    System.out.println(m);
   }
 }
