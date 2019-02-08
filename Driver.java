81e08b56f25cfd1d93e3530494f946a95dd20ccc
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-08 16:58:24 -0500

    modified driver to test boards from size 1-10

--- Driver.java
+++ Driver.java
@@ -1,30 +1,52 @@
 public class Driver{
   public static void main(String[] args) {
-    QueenBoard m = new QueenBoard(9);
-    //System.out.println(m);
-    /*m.addQueen(4,4);
-    System.out.println(m);
-    m.addQueen(1,8);
-    System.out.println(m);
-    //m.removeQueen(4,4); // should also remove threatened by the queen @ 4,4
-    System.out.println(m);*/
-    m.solve();
-    System.out.println(m);
-    System.out.println(m.countSolutions()); //46 F 352 all
+    String x = " solutions found\n";
+    String z = "First solution found:\n\n";
     QueenBoard a = new QueenBoard(0);
-    System.out.println(a.countSolutions()); //0 0
+      System.out.println(a.countSolutions() + x); //0
+      a.solve();
+      System.out.println(z + a);
     QueenBoard b = new QueenBoard(1);
-    System.out.println(b.countSolutions());//1 1
+      System.out.println(b.countSolutions() + x);//1
+      b.solve();
+      System.out.println(z + b);
     QueenBoard c = new QueenBoard(2);
-    System.out.println(c.countSolutions());//0 0
+      System.out.println(c.countSolutions() + x);//0
+      c.solve();
+      System.out.println(z + c);
     QueenBoard d = new QueenBoard(3);
-    System.out.println(d.countSolutions());//0 0
+      System.out.println(d.countSolutions() + x);//0
+      d.solve();
+      System.out.println(z + d);
     QueenBoard e = new QueenBoard(4);
-    System.out.println(e.countSolutions());//1 2
+      System.out.println(e.countSolutions() + x);//2
+      e.solve();
+      System.out.println(z + e);
     QueenBoard f = new QueenBoard(5);
-    System.out.println(f.countSolutions());//2 10
+      System.out.println(f.countSolutions() + x);//10
+      f.solve();
+      System.out.println(z + f);
     QueenBoard g = new QueenBoard(6);
-    System.out.println(g.countSolutions());//1 4
+      System.out.println(g.countSolutions() + x);//4
+      g.solve();
+      System.out.println(z + g);
+    QueenBoard h = new QueenBoard(7);
+      System.out.println(h.countSolutions() + x);//40
+      h.solve();
+      System.out.println(z + h);
+    QueenBoard i = new QueenBoard(8);
+      System.out.println(i.countSolutions() + x);//92
+      i.solve();
+      System.out.println(z + i);
+    QueenBoard j = new QueenBoard(9);
+      System.out.println(j.countSolutions() + x); //352
+      j.solve();
+      System.out.println(z + j);
+    QueenBoard k = new QueenBoard(10);
+      System.out.println(k.countSolutions() + x);//724
+      //System.out.println(k);
+      k.solve();
+      System.out.println(z + k);
 
   }
 }
