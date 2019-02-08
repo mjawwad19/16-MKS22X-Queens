45dcc7eb27c0ef4e3546526300f43db0a3bd4393
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-08 00:18:34 -0500

    checked removal with diff size board

--- Driver.java
+++ Driver.java
@@ -6,7 +6,7 @@ public class Driver{
     System.out.println(m);
     m.addQueen(1,8);
     System.out.println(m);
-    m.removeQueen(4,4); // should also remove threatened by the queen @ 4,4
+    //m.removeQueen(4,4); // should also remove threatened by the queen @ 4,4
     System.out.println(m);
   }
 }
