0869a4d889485470b0f200e3459ce71c8c6eda29
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-08 14:35:18 -0500

    succesfully find a solution.

--- QueenBoard.java
+++ QueenBoard.java
@@ -105,10 +105,9 @@ public class QueenBoard{
     String out = "";
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board.length; j++) {
-        if (board[i][j] == 0) out += "_ ";
+        if (board[i][j] >= 0) out += "_ ";
         if (board[i][j] == -1) out += "Q ";
-        //z
-        if (board[i][j] > 0) out += "" + board[i][j] + " ";
+        //if (board[i][j] > 0) out += "" + board[i][j] + " ";
       }
       out += "\n";
     }
@@ -132,12 +131,14 @@ public class QueenBoard{
 
   private boolean solveH(int col) throws IllegalArgumentException {
     if (col < 0) throw new IllegalArgumentException();
-    if (col == board.length -1) return true;
+    if (col == board.length) return true;
     for (int i = 0; i < board.length; i++) {
       if (addQueen(i, col)) {
-        return solveH(col+1);
+        if (solveH(col+1)) {
+          return true;
+        }
       }
-      else {removeQueen(i,col);}
+      removeQueen(i,col);
     }
     return false;
   }
