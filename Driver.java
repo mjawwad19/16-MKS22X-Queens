449f8ec0bbc5a3c48804b8c38664934f14d6a00c
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-08 00:11:36 -0500

    added test cases for removing a queen

--- Driver.java
+++ Driver.java
@@ -1,8 +1,12 @@
 public class Driver{
   public static void main(String[] args) {
-    QueenBoard m = new QueenBoard(8);
+    QueenBoard m = new QueenBoard(9);
     System.out.println(m);
-    m.addQueen(2,1); //currently should block off threatened same row.
+    m.addQueen(4,4);
+    System.out.println(m);
+    m.addQueen(1,8);
+    System.out.println(m);
+    m.removeQueen(4,4); // should also remove threatened by the queen @ 4,4
     System.out.println(m);
   }
 }
